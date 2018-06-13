d3617e8903d6875405d622b50c44ab8300b222a2
Maria Morales <maremarismaria@gmail.com>
2018-06-13 20:57:39 +0100

    Add utility functions for MongoDB

---
+++ utils/mongoUtils.js
@@ -0,0 +1,33 @@
+
+const ObjectId = require('mongodb').ObjectId;
+
+function mongoUtils (MongoDB) {
+  return {
+    /**
+     * Converts a numerical connection status into literal
+     * @param {number} mongooseStatus Mongoose connection state
+     * @returns {string} Literal database status
+     */
+    literalStatus (mongooseStatus){
+      const literalStatuses = [ 'disconnected', 'connected', 'connecting', 'disconnecting' ];
+      return literalStatuses[mongooseStatus];
+    },
+    /**
+     * Converts the value of the '_id' property into a MongoDB ObjectId
+     * @param {object} criteria Criteria or filters that identify a MongoDB document
+     * @returns {object} criteria
+     */
+    parseObjectId (criteria){
+      let mongoId = Object.keys(criteria).filter(key => key === '_id');
+
+      if(mongoId.length) {
+        let [id] = mongoId;
+        criteria[id] = new ObjectId(criteria[id]);
+      }
+
+      return criteria;
+    }
+  }
+}
+
+module.exports = mongoUtils;
