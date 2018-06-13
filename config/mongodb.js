00c7641fda5037aca372e62670f9663f16396525
Maria Morales <maremarismaria@gmail.com>
2018-06-13 20:51:56 +0100

    Update MongoDB config

--- config/mongodb.js
+++ config/mongodb.js
@@ -1,9 +1,7 @@
 
 const config = {
-  uri: "mongodb://localhost:27017/dbtodo" || process.env.MONGO_URI,
-  options : {
-    useMongoClient: true
-  }
+  uri: 'mongodb://localhost:27017' || process.env.MONGO_URI,
+  database: 'dbtodo' || process.env.MONGO_DATABASE
 };
 
 module.exports = config;
